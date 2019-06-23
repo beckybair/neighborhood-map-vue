16786a80975d98327a6e83e8d80788de0e8750d1
beckybair <beckyhowen@gmail.com>
2019-06-23 08:25:00 -0500

    Initial dist subtree commit

---
+++ precache-manifest.40bc96b44afafb3078d98af8c0bebdec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/neighborhood-map-vue/robots.txt"
+  },
+  {
+    "revision": "9c3680c2de4699502094",
+    "url": "/neighborhood-map-vue/js/chunk-vendors.25c8511d.js"
+  },
+  {
+    "revision": "c808729478937416d020",
+    "url": "/neighborhood-map-vue/js/app.72bc2045.js"
+  },
+  {
+    "revision": "5e36e56ca28dee503bd1",
+    "url": "/neighborhood-map-vue/js/about.5c9e0f63.js"
+  },
+  {
+    "revision": "72d8ea50a8ee7110e96fa48f77b27e54",
+    "url": "/neighborhood-map-vue/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/neighborhood-map-vue/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c808729478937416d020",
+    "url": "/neighborhood-map-vue/css/app.d57b8a38.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/neighborhood-map-vue/precache-manifest.40bc96b44afafb3078d98af8c0bebdec.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "neighborhood-map"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
